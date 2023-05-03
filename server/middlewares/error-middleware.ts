73aa944adde7a2f39c32b983dae29ded93ed6b37
Vlad <vla.koncha@gmail.com>
2023-05-03 14:11:01 +0700

    error response

--- server/middlewares/error-middleware.ts
+++ server/middlewares/error-middleware.ts
@@ -2,13 +2,12 @@ import {ApiError} from "../exceptions/api-error";
 import {NextFunction, Request, Response} from "express";
 
 
-export default function (err: ApiError | ErrorConstructor,
+export default function (err: ApiError,
                          req: Request,
                          res: Response,
                          next: NextFunction): Response {
     console.log(err);
 
-    err = err as ApiError;
     const status: number = err.status || 500;
     const message: string = err.message || 'Something went wrong';
     const errors: any[] = err.errors || [];
