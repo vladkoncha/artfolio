943e7d7a1a76c8eff7922f4a92a9a33723c80e34
Vlad <vla.koncha@gmail.com>
2023-05-08 11:58:54 +0700

    adds Loader

---
+++ client/src/components/loader/Loader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Loader.module.css'
+
+const Loader = () => {
+    return (
+        <div className={classes.loaderContainer}>
+            <div className={classes.loader}>
+            </div>
+        </div>
+
+    );
+};
+
+export default Loader;
